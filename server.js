3b376a74fbd1ca356c8f1428a6e158b11928d94c
vandhanad779-glitch <21951a67h0@iare.ac.in>
2026-03-02 15:00:30 +0530

    Magodb is sccessfuly connected

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", require("./routes/authRoutes"));
+app.use("/api", require("./routes/productRoutes"));
+app.use("/api", require("./routes/cartRoutes"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
